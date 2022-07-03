1c76535e7659947ced6fdbd81b380ef3f880b450
AthanasiosLappas1 <107856756+AthanasiosLappas1@users.noreply.github.com>
2022-07-03 11:32:54 -0400

    Add files via upload

---
+++ Turtle_Graphics_Cir le_To_Line_And_Expansion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+turtl1 = turtle.Turtle()
+screen = turtle.Screen()
+turtl1.speed(10)
+for i in range(1,1911):
+    turtl1.forward(i)
+    turtl1.left(i)
